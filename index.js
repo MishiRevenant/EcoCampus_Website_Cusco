770db89fa195ee40df950665cc7f2658298f428e
Fcranz28 <franzaguilar28@gmail.com>
2025-08-20 22:09:07 -0500

    Agrego mi parte(Franz)

--- index.js
+++ index.js
@@ -28,3 +28,66 @@ const page=(location.pathname.split('/').pop()||'index.html').replace('.html',''
 const active=document.getElementById('nav-'+page);
 if(active) active.classList.add('active');
 if(document.getElementById('barChart')){const ctx=document.getElementById('barChart').getContext('2d');new Chart(ctx,{type:'bar',data:{labels:['Residuos','Papel','CO₂'],datasets:[{data:[9.4,3.1,16],backgroundColor:'#1982c4'}]},options:{plugins:{legend:{display:false}},scales:{y:{beginAtZero:true}}}});}
+
+
+/* Estas son las funciones para el sweetalert */
+
+function alertForo(){
+  Swal.fire({
+    title: "Drag me!",
+    icon: "success",
+    draggable: true
+  });
+}
+function alertTaza(){
+Swal.fire({
+  imageUrl: "https://i.postimg.cc/tgDJ9Gkf/Captura-de-pantalla-2025-08-20-213600.png",
+  imageHeight: 400,
+  imageAlt: "A tall image"
+});
+}
+function alertBolsa(){
+Swal.fire({
+  imageUrl: "https://i.postimg.cc/rz6FfVyd/Captura-de-pantalla-2025-08-20-213607.png",
+  imageHeight: 400,
+  imageAlt: "A tall image"
+});
+}
+function alertStickers(){
+  Swal.fire({
+    title: "¿Estas Seguro que quieres canjear los Stickers?",
+    text: "SI QUIERO LOS STICKERS!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "SI!"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Listo",
+        text: "Acabas de canjear los Stickers.",
+        icon: "success"
+      });
+    }
+  });
+}
+function alertCuaderno(){
+  Swal.fire({
+    title: "¿Estas Seguro que quieres canjear el Cuaderno?",
+    text: "SI QUIERO EL CUADERNO!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "SI!"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Listo",
+        text: "Acabas de canjear el Cuaderno.",
+        icon: "success"
+      });
+    }
+  });
+}
